e9765186b20675b9004c5d49b8d18630a302e9aa
Anacoronel <anacoronel@hotmail.com>
2021-11-14 18:52:21 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function injectCss(file = 'style.css') {
+  fetch(file)
+    .then((response) => response.text())
+    .then((data) => {
+      let stl = document.createElement('style');
+      stl.innerHTML = data;
+      document.head.appendChild(stl);
+    });
+}
+
+function injectAll() {
+  let links = document.getElementsByTagName('link');
+  for (stl of links) {
+    injectCss(stl.href);
+  }
+}
+
+window.onload = injectAll;
